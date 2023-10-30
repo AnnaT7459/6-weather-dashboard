7a408eaef1542e911853fbbd5ac4186f01b2e43c
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-10-29 20:06:46 -0500

    styled webpage so that the current day is separate from the 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var weatherAPI = `api.openweathermap.org/data/2.5/forecast?q=dallas&appid=790c21a3a3e4e9fbeed5495456bb6293`
+var weatherAPI = `api.openweathermap.org/data/2.5/forecast?units=imperial&q=dallas&appid=790c21a3a3e4e9fbeed5495456bb6293`
 console.log(weatherAPI)
 // help from H. Burke
 
